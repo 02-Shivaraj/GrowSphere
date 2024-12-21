04dc58968af96225f47f8b6c77c7af913f1bde98
shivaraj <shivaraj.a.2611@gmail.com>
2024-12-22 02:03:26 +0530

    updated frontend

---
+++ FrontEnd/assets/bg.js
@@ -0,0 +1,16 @@
+// window.bg = 0;
+// script1.js
+localStorage.setItem('bg', 0);
+
+
+// function changeVariable(newValue) {
+//     bg = newValue; // Change the value of myVariable
+//     localStorage.setItem('bg',newValue);
+// }
+
+// function getVariable() {
+//     return localStorage.getItem('bg'); // Return the current value of myVariable
+// }
+
+window.changeVariable = changeVariable; // Make the function globally accessible
+window.getVariable = getVariable;
